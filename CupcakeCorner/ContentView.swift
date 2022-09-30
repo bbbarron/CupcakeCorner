53e58fea7aa6a174c87b2900a6a99fa0075c0548
Barry Barron <barrybbarron@gmail.com>
2022-09-30 16:04:20 -0700

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Barry Barron
+//  September 30, 2022
+//  based on "Cupcake Corner"
+//  by Paul Hudson.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject var order = Order()
+
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3...20)
+                }
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+
+                Section {
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Delivery details")
+                    }
+                }
+            }
+            .navigationTitle("Cupcake Corner")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
